4b6ff6105968e8059dab8f36e0764f2039ecff36
Nathan Fearnley <nfearnley@gmail.com>
2012-02-15 05:44:08 -0500

    Began trying to modify config file

--- src/com/slugsource/steamcategories/lib/SteamCategories.java
+++ src/com/slugsource/steamcategories/lib/SteamCategories.java
@@ -4,6 +4,10 @@
  */
 package com.slugsource.steamcategories.lib;
 
+import com.slugsource.steamcategories.lib.vdf.Node;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 /**
  *
  * @author Nathan Fearnley
@@ -12,5 +16,19 @@ public class SteamCategories
 {
     public static void main(String[] args)
     {
+        // Load the file
+        Node rootNode;
+        File file = new File("F:\\Programming\\SteamCategories\\sharedconfig.vdf");
+        try
+        {
+            rootNode = Node.readFromFile(file);
+        } catch (FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+            return;
+        }
+        
+        System.out.println(rootNode.getName());
+        rootNode.getNode("UserLocalConfigStore");
     }
 }
